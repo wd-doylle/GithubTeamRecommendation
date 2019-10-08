3478bed6c3fcb17423c88bcc50bfc2e6df2abc6e
wd-doylle <18221943430@163.com>
2019-10-08 20:59:54 +0800

    commit

---
+++ recommend_euclidean_dis.py
@@ -0,0 +1,78 @@
+import json
+import pandas as pd
+
+repo_profiles = {}
+with open('repo_profiles.json') as rj:
+    for rl in rj.readlines():
+        line = rl.split('\t')
+        repo = line[0]
+        profile = json.loads(line[1])
+        repo_profiles[repo] = profile
+    
+team_profiles = []
+teams = []
+with open('team_profiles.json') as tj:
+    for tl in tj.readlines():
+        line = tl.split('\t')
+        team = json.loads(line[0])
+        profile = json.loads(line[1])
+        team_profiles.append(profile)
+        teams.append(team)
+
+repo_profiles_df = pd.DataFrame(repo_profiles).transpose()
+repo_profiles_df.fillna('',inplace=True)
+team_profiles_df = pd.DataFrame(team_profiles,index=[str(t) for t in teams])
+team_profiles_df.rename(columns={
+                                   'repo_size':'size',
+                                   'repo_forks':'forks',
+                                   'repo_subscribers':'subscribers',
+                                   'repo_watchers':'watchers',
+                                   'languages':'languages',
+                                   'topics':'topics'
+                               },inplace=True)
+
+numerics = ['size','forks','subscribers','watchers']
+
+min = repo_profiles_df[numerics].min()
+max = repo_profiles_df[numerics].max()
+repo_profiles_df[numerics] = (repo_profiles_df[numerics]-min)/(max-min)
+team_profiles_df[numerics] = (team_profiles_df[numerics]-min)/(max-min)
+
+import numpy as np
+
+def euclidean_distance(p1,p2):
+    distance = np.linalg.norm(p1[numerics]-p2[numerics])**2
+    langs = set(p1['languages']).union(p2['languages'])
+    topics = set(p1['topics']).union(p2['topics'])
+    p1_langs = pd.Series([lang in p1['languages'] for lang in langs])/np.sqrt(len(langs))
+    p2_langs = pd.Series([lang in p2['languages'] for lang in langs])/np.sqrt(len(langs))
+    p1_topics = pd.Series([topic in p1['topics'] for topic in topics])/np.sqrt(len(topics))
+    p2_topics = pd.Series([topic in p2['topics'] for topic in topics])/np.sqrt(len(topics))
+    distance += np.linalg.norm(p1_langs-p2_langs)**2 + np.linalg.norm(p1_topics-p2_topics)**2
+    distance = np.sqrt(distance)
+    
+    return distance
+
+from queue import PriorityQueue
+
+
+with open('recommenda_euclidean.json','w') as oj:
+    cnt = 0
+    for repo,repo_profile in repo_profiles_df.iterrows():
+        cnt += 1
+        # if cnt > 10:
+        #     break
+        print(cnt)
+        rec = []
+        queue = PriorityQueue()
+        for team,team_profile in team_profiles_df.iterrows():
+            dis = euclidean_distance(repo_profile,team_profile)
+            queue.put_nowait((-dis,team))
+            if queue.qsize() > 10:
+                queue.get_nowait()
+        while queue.qsize()>0:
+            rec.append(queue.get_nowait())
+        oj.write(repo+'\t')
+        for tm in rec:
+            oj.write(json.dumps(tm)+'\t')
+        oj.write('\n')
