f826c8334552729528f4d56c7d14aeb6464d5536
wd-doylle <18221943430@163.com>
2019-10-29 21:48:26 +0800

    Performance fix

--- recommend_euclidean_dis.py
+++ recommend_euclidean_dis.py
@@ -36,33 +36,41 @@ team_profiles_df.rename(columns={
 numerics = ['size','forks','subscribers','watchers']
 non_numerics = ['languages','topics']
 
-min = repo_profiles_df[numerics].min()
-max = repo_profiles_df[numerics].max()
-repo_profiles_df[numerics] = (repo_profiles_df[numerics]-min)/(max-min)
-team_profiles_df[numerics] = (team_profiles_df[numerics]-min)/(max-min)
+minn = repo_profiles_df[numerics].min()
+maxx = repo_profiles_df[numerics].max()
+repo_profiles_df[numerics] = (repo_profiles_df[numerics]-minn)/(maxx-minn)
+team_profiles_df[numerics] = (team_profiles_df[numerics]-minn)/(maxx-minn)
 
 import numpy as np
-
+import difflib
 
 euclidean_numerics_v = np.vectorize(
     lambda p1,p2: np.linalg.norm(p1-p2)**2,signature="(n),(n)->()"
 )
 def euclidean_non_numerics(p1,p2):
-    langs = set(p1[0]).union(p2[0])
-    topics = set(p1[1]).union(p2[1])
-    p1_langs = pd.Series([lang in p1[0] for lang in langs])/np.sqrt(len(langs))
-    p2_langs = pd.Series([lang in p2[0] for lang in langs])/np.sqrt(len(langs))
-    p1_topics = pd.Series([topic in p1[1] for topic in topics])/np.sqrt(len(topics))
-    p2_topics = pd.Series([topic in p2[1] for topic in topics])/np.sqrt(len(topics))
-    return np.linalg.norm(p1_langs-p2_langs)**2 + np.linalg.norm(p1_topics-p2_topics)**2
+    sm_langs = difflib.SequenceMatcher(None,p1[0],p2[0])
+    sm_topics = difflib.SequenceMatcher(None,p1[1],p2[1])
+    return sm_langs.ratio()**2 + sm_topics.ratio()**2
 euclidean_non_numerics_v =  np.vectorize(euclidean_non_numerics,signature="(n),(n)->()")
 euclidean_v = np.vectorize(lambda num,non_num:np.sqrt(num+non_num))
 
+def sort_to_k(ary,k,key=lambda x:x,reversed=False):
+    k = min(k,len(ary))
+    for i in range(k):
+        for j in range(len(ary)-1-i):
+            if not reversed:
+                if key(ary[len(ary)-1-j]) < key(ary[len(ary)-2-j]):
+                    ary[len(ary)-1-j],ary[len(ary)-2-j] = ary[len(ary)-2-j],ary[len(ary)-1-j]
+            else:
+                if key(ary[len(ary)-1-j]) > key(ary[len(ary)-2-j]):
+                    ary[len(ary)-1-j],ary[len(ary)-2-j] = ary[len(ary)-2-j],ary[len(ary)-1-j]
+    return ary
 
 with open('recommend_euclidean.json','a') as oj:
     with open('recommend_euclidean.bp','r+') as rb:
         bp = int(rb.read())
         cnt = 0
+        k = 50
         for repo,repo_profile in repo_profiles_df.iterrows():
             if cnt < bp:
                 cnt += 1
@@ -73,7 +81,7 @@ with open('recommend_euclidean.json','a') as oj:
             dis_num = euclidean_numerics_v(repo_profile[numerics],team_profiles_df.loc[teams,numerics])
             dis_non_num = euclidean_non_numerics_v(repo_profile[non_numerics],team_profiles_df.loc[teams,non_numerics])
             dis = euclidean_v(dis_num,dis_non_num)
-            idxs = sorted(list(range(len(teams))),key=lambda i:dis[i])
+            idxs = sort_to_k(list(range(len(teams))),k,key=lambda i:dis[i])
             oj.write(repo+'\t')
             for i in range(50):
                 oj.write(json.dumps(teams[idxs[i]])+'\t')
---
+++ repo_graph.py
@@ -0,0 +1,55 @@
+import json
+import pandas as pd
+import numpy as np
+import difflib
+import torch
+
+repo_profiles = {}
+repo_teams = {}
+with open('repo_profiles_new.json') as rj:
+    for rl in rj.readlines():
+        line = rl.split('\t')
+        repo = line[0]
+        profile = json.loads(line[1])
+        repo_profiles[repo] = profile
+        repo_teams[repo] = repo_profiles[repo].pop('teams')
+        repo_profiles[repo]
+
+repo_profiles = pd.DataFrame(repo_profiles).transpose()
+repo_profiles.fillna('',inplace=True)
+
+numerics = ['size','forks','subscribers','watchers']
+non_numerics = ['languages','topics']
+
+minn = repo_profiles[numerics].min()
+maxx = repo_profiles[numerics].max()
+repo_profiles[numerics] = (repo_profiles[numerics]-minn)/(maxx-minn)
+repo_profiles[numerics] = (repo_profiles[numerics]-minn)/(maxx-minn)
+
+
+def euclidean_non_numerics(p1,p2):
+    sm_langs = difflib.SequenceMatcher(None,p1[0],p2[0])
+    sm_topics = difflib.SequenceMatcher(None,p1[1],p2[1])
+    return sm_langs.ratio()**2 + sm_topics.ratio()**2
+euclidean_non_numerics_v =  np.vectorize(euclidean_non_numerics,signature="(n),(n)->()")
+
+repos = repo_profiles.index
+
+cuda0 = torch.device('cuda:0')
+repo_num = [repo_profiles[numerics].values]*repo_profiles.shape[0]
+repo_num = torch.tensor(repo_num,device=cuda0,requires_grad=False)
+dis_num = ((repo_num.transpose(0,1)-repo_num)**2).sum(2)
+
+cnt = 0
+dis_non_num = []
+for repo,repo_profile in repo_profiles.iterrows():
+    cnt += 1
+    print(cnt)
+    dis_non_num.append(euclidean_non_numerics_v(repo_profile[non_numerics],repo_profiles[non_numerics]))
+    dis_non_num = torch.tensor(dis_non_num,device=cuda0,requires_grad=False)
+dis = dis_num+dis_non_num
+dis = dis.cpu().numpy()
+k = 50
+with open('repo_graph_new.json','w') as rj:
+    for i,d in enumerate(dis):
+        rj.write("%s\t%s\n"%(repos[i],json.dumps(d)))
