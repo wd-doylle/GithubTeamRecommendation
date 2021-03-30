94299ca1cb408644b592be739cf13eaa5c1bf5a5
Duo Wang <wangduo@agora.io>
2021-03-30 14:45:19 +0800

    add linux script

---
+++ ind2name.sh
@@ -0,0 +1,17 @@
+# convert indices to names in recommendation results
+python recommend_ind2name.py gat_team_score_5.json recommend_gat_5.json
+python recommend_ind2name.py gat_team_score_10.json recommend_gat_10.json
+python recommend_ind2name.py gat_team_score_30.json recommend_gat_30.json
+python recommend_ind2name.py gat_team_score_50.json recommend_gat_50.json
+python recommend_ind2name.py gts_alt_5.json recommend_gat_alt1_5.json
+python recommend_ind2name.py gts_alt_10.json recommend_gat_alt1_10.json
+python recommend_ind2name.py gts_alt_30.json recommend_gat_alt1_30.json
+python recommend_ind2name.py gts_alt_50.json recommend_gat_alt1_50.json
+python recommend_ind2name.py gts_alt2_5.json recommend_gat_alt2_5.json
+python recommend_ind2name.py gts_alt2_10.json recommend_gat_alt2_10.json
+python recommend_ind2name.py gts_alt2_30.json recommend_gat_alt2_30.json
+python recommend_ind2name.py gts_alt2_50.json recommend_gat_alt2_50.json
+python recommend_ind2name.py soagree_team_score_5.json recommend_soagree_5.json
+python recommend_ind2name.py soagree_team_score_10.json recommend_soagree_10.json
+python recommend_ind2name.py soagree_team_score_30.json recommend_soagree_30.json
+python recommend_ind2name.py soagree_team_score_50.json recommend_soagree_50.json
